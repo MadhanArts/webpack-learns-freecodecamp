c64667525aa430586f4dc33ee775b48d868e0ec9
MadhanArts <madhanartsmadhan@gmail.com>
2023-02-27 19:59:32 +0530

    cachebusting html-webpack-plugin sass-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./main.css";
+import "./main.scss";
 const { run } = require("./app/app");
 
 run();
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,49 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   //   devtool: 'none',
   entry: './src/index.js',
   output: {
-    filename: 'master.js',
+    filename: 'master.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-
   module: {
-
     // loader loads in reverse order
     rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     {
+      //       loader: 'style-loader', // inject loaded css webpack to DOM
+      //     },
+      //     {
+      //       loader: 'css-loader', // to convert css file to commonjs
+      //     },
+      //   ],
+      // },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
           {
             loader: 'style-loader', // inject loaded css webpack to DOM
           },
           {
-            loader: 'css-loader', // to convert css file to webpack
+            loader: 'css-loader', // to convert css file to commonjs
+          },
+          {
+            loader: 'sass-loader', // to convert sass file to css
           },
         ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      inject: 'body'
+    }),
+  ],
 };
